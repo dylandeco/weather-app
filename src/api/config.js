059fbf138ed00ad3d0bec53b8d9dfbf13d796572
Dylan DeCoursey <decourse@ualberta.ca>
2021-09-19 17:26:15 -0600

    test

---
+++ src/api/config.js
@@ -0,0 +1,5 @@
+// API KEY AND URL
+export const API_KEY = process.env.REACT_APP_API_KEY;
+export const API_URL = `https://api.openweathermap.org/`;
+
+export const MAP_API_KEY = 'AIzaSyCetXJB_tRH-EbzpQ3n4Wkn7wRX4xuPpLA';
