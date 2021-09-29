c6a9fd357738da1579d489b9c14e8a7caa3d3bb1
Dylan DeCoursey <decourse@ualberta.ca>
2021-09-29 12:17:55 -0600

    Places

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -1,45 +1,11 @@
 import { Helmet } from 'react-helmet';
-import { useState, useEffect } from 'react';
-import { Box, Container, Grid, Paper } from '@material-ui/core';
 
 const Map = () => {
-  function FormRow() {
-    return (
-      //return renders the grid
-      <>
-        <Grid item xs={4}>
-          <Paper>item</Paper>
-        </Grid>
-        <Grid item xs={4}>
-          <Paper>item</Paper>
-        </Grid>
-        <Grid item xs={4}>
-          <Paper>item</Paper>
-        </Grid>
-      </>
-    );
-  }
-
   return (
     <>
       <Helmet>
         <title>Map | Weather App</title>
       </Helmet>
-
-      <Box
-        sx={{
-          backgroundColor: 'background.default',
-          minHeight: '100%'
-        }}
-      >
-        <Container sx={{ height: '100%', width: '100%' }}>
-          <Grid container direction="column" spacing={1}>
-            <Grid container item direction="column" xs={12} spacing={3}>
-              <FormRow />
-            </Grid>
-          </Grid>
-        </Container>
-      </Box>
     </>
   );
 };
